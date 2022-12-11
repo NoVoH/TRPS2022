d581748314d897e4f69a53ff76f53ba7bfc3fa57
NoVoH <62092912+NoVoH@users.noreply.github.com>
2022-12-11 21:14:42 +0200

    Closed #1

--- LR4/lab4.py
+++ LR4/lab4.py
@@ -198,7 +198,7 @@ class Map:
         if self.grid[x][y + 1] is not None:
             neighbours.append(self.grid[x][y + 1])
         if self.grid[x][y - 1] is not None:
-            neighbours.append(self.grid[x][y + 1])
+            neighbours.append(self.grid[x][y - 1])
         if self.grid[x + 1][y] is not None:
             neighbours.append(self.grid[x + 1][y])
         if self.grid[x - 1][y] is not None:
